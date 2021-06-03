8615fcfce85b021317978f8105fb62b32f046036
Jani <pappjanos.munka@gmail.com>
2021-06-03 23:50:01 +0200

    Search keywords given as parameter implemented

---
+++ main.py
@@ -0,0 +1,17 @@
+import selenium
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(r"C:/Python39/Scripts/chromedriver.exe")
+
+driver.get('https://www.google.hu/?hl=hu')
+
+accept_button = driver.find_element_by_id('L2AGLb')
+time.sleep(1) 
+accept_button.click()
+
+input_field = driver.find_element_by_name('q')
+input_field.send_keys(sys.argv[1])
+input_field.send_keys(Keys.RETURN)
